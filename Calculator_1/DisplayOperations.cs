8d4999f09b1f6beae56ff1cb1d641a2678b7a7f9
Jason Thatcher <contact@jasonthatcher.com>
2018-01-13 11:50:47 -0600

    Add display ops class

---
+++ Calculator_1/DisplayOperations.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_1
+{
+    class DisplayOperations
+    {
+        string displayText;
+        string formattedText;
+        bool decimalPresent = false;
+        bool multiDecimalPresent = false;
+        bool tooBig = false, exp = false, needsComma = false;
+        int decimalPosition = 0, leftOfDecimalLength = 0, rightOfDecimalLength = 0;
+        bool isANumber = true;
+        
+        int characterLimit = 19; //limits the length of number left / right of decimal in calc...
+        int displayLimit = 8; //Sets the display Limit will display exponent if over limit....
+
+        public DisplayOperations()
+        {
+
+        }
+
+        public DisplayOperations(string text)
+        {
+            displayText = text;
+        }
+
+        public DisplayOperations(string text, bool isANumber)
+        {
+            displayText = text;
+            this.isANumber = isANumber;
+        }
+
+        public DisplayOperations(int maxStringLength, int maxDisplayStringLength)
+        {
+            characterLimit = maxStringLength;
+            displayLimit = maxDisplayStringLength;
+        }
+
+        public DisplayOperations(string text, int maxStringLength, int maxDisplayStringLength)
+        {
+            displayText = text;
+            characterLimit = maxStringLength;
+            displayLimit = maxDisplayStringLength;
+        }
+        
+        public DisplayOperations(string text, bool isANumber, int maxDisplayStringLength)
+        {
+            displayText = text;
+            this.isANumber = isANumber;
+            displayLimit = maxDisplayStringLength;
+        }
+
+        public DisplayOperations(string text, bool isANumber, int maxStringLength, int maxDisplayStringLength)
+        {
+            displayText = text;
+            this.isANumber = isANumber;
+            characterLimit = maxStringLength;
+            displayLimit = maxDisplayStringLength;
+        }
+
+        public string GetFormattedDisplayText(string text, bool isANumber, int maxStringLength, int maxDisplayStringLength)
+        {
+            displayText = text;
+            this.isANumber = isANumber;
+            characterLimit = maxStringLength;
+            displayLimit = maxDisplayStringLength;
+            ValidateDisplayText();
+
+            return FormattedText;
+        }
+
+        public string GetFormattedDisplayText(string text, int maxStringLength, int maxDisplayStringLength)
+        {
+            displayText = text;
+            characterLimit = maxStringLength;
+            displayLimit = maxDisplayStringLength;
+            ValidateDisplayText();
+
+            return FormattedText;
+        }
+
+        public string GetFormattedDisplayText(string text, bool isANumber)
+        {
+            displayText = text;
+            this.isANumber = isANumber;
+            ValidateDisplayText();
+
+            return FormattedText;
+        }
+        
+        public string FormattedText { get => formattedText; set => formattedText = value; }
+
+        //Get Decimal Position, if any
+        private void GetDecimalInformation()
+        {
+            if (displayText.Contains('.'))
+            {
+                if (displayText.IndexOf('.') == displayText.LastIndexOf('.'))
+                {
+                    decimalPresent = true;
+                    decimalPosition = displayText.IndexOf('.');
+                    leftOfDecimalLength = decimalPosition - 1;
+                    rightOfDecimalLength = displayText.Length - decimalPosition - 1;
+                }
+                else
+                {
+                    multiDecimalPresent = true;
+                    isANumber = false;
+                    FormattedText = "too many '.'";
+                }
+            }            
+        }
+        
+        //Display rules and check for violation....
+        private void ValidateDisplayText()
+        {
+            GetDecimalInformation();
+
+            if (isANumber)
+            {
+
+                if (displayText.Length > characterLimit)
+                {
+                    tooBig = true;
+                    FormattedText = "num too big";
+                }
+                if (displayText.Length > displayLimit)
+                {
+                    exp = true;
+                    CreateExponent();
+                }
+                if (decimalPresent && leftOfDecimalLength > 3)
+                {
+                    needsComma = true;
+                    InsertComma();
+                }
+                else if(!decimalPresent && displayText.Length > 3)
+                {
+                    needsComma = true;
+                    InsertComma();
+                }
+            }
+        }
+
+        private void CreateExponent()
+        {   
+            string eRight = DefineRightOfExp();
+            string eLeft = DefineLeftOfExp();
+
+            FormattedText = eLeft + "e" + eRight;
+        }
+
+        private string DefineRightOfExp()
+        {
+            double e;
+            string workingText = displayText;
+
+            if (leftOfDecimalLength > 0)
+            {
+                int fromPosition = decimalPosition;
+                int toPosition = 1;
+                e = fromPosition - toPosition;                
+            }
+            else
+            {
+                int firstNonZeroNumber = 0;
+
+                do
+                {
+                   firstNonZeroNumber += 1;
+                } while (workingText[firstNonZeroNumber].Equals("0"));
+                
+                int toPosition = firstNonZeroNumber;
+                int fromPosition = 0;
+
+                e = fromPosition - toPosition;
+            }
+
+            return e.ToString();
+        }
+
+        private string DefineLeftOfExp()
+        {
+            //evaluate string.  Use Exponential Notation 5.1234E4.
+            double d = Convert.ToDouble(displayText);
+            string result = displayText.Remove(displayLimit);            
+
+            if (d >= 1.000)
+            {
+                double e = Convert.ToDouble(result);
+                e = Math.Pow(e, displayLimit - 1);
+                e = Math.Round(e, displayLimit - 2);
+                result = e.ToString();
+            }
+            else
+            {
+                int firstNonZeroNumber = 0;
+                do
+                {
+                    firstNonZeroNumber += 1;
+                } while (result[firstNonZeroNumber].Equals("0"));
+
+                result.Remove(0, firstNonZeroNumber);
+                result.Insert(1, ".");                
+            }
+
+            return result;
+        } 
+
+        private void InsertComma()
+        {
+            int commaPosition = 3; //if 3, commas will be inserted every 3rd character.
+            int holder;
+            FormattedText = displayText;
+
+            if (!decimalPresent)
+            {
+                holder = FormattedText.Length;
+            }
+            else
+            {
+                holder = decimalPosition;
+            }
+
+            do
+            {
+                holder -= commaPosition; //"12345" length = 5 & string is an array of char so end is 4 (0-4)
+                FormattedText.Insert(holder, ","); // using comment above 5-3 inserts at array[3] or moves 3 over and inserts comma between 2 & 3...12,345
+
+            } while (holder > 0);                        
+        }
+    }  
+}
--- Calculator_1/Form1.cs
+++ Calculator_1/Form1.cs
@@ -13,6 +13,7 @@ namespace Calculator_1
         const int characterLimit = 19; //limits the length of number left / right of decimal in calc...
         const int displayLimit = 8; //Sets the display Limit will display exponent if over limit....
         CalculatorOps Calc;
+        DisplayOperations Disp;
         StringBuilder displayText;
 
         public Form1()
@@ -21,28 +22,6 @@ namespace Calculator_1
             displayDirty = false;
             displayText = new StringBuilder("0");
             InitializeComponent();
-        }
-        
-        private void UpdateDisplayString(char key)
-        {
-            //Validate character...
-            if (!Calc.CharacterAllowed(key))
-            {
-                MessageBox.Show("Invalid Input. You entered: " + key.ToString());
-            }     
-            
-            //Remove leading zeros...
-            if(displayText.Length>0 && displayText[0] == '0')
-            {
-                displayText.Remove(0, 1);
-            }
-
-            //Add character to stringbuilder...
-            displayText.Append(key);
-
-            //Update display with sb value.
-            WriteToDisplay(displayText.ToString());         
-            
         }
         
         private void EvaluateOperatorKeyPress(char c) //operation key (+,*,-,/,=)  pressed.  
@@ -76,7 +55,29 @@ namespace Calculator_1
             //Record new ops to be performed according to key press.
             ops = c;            
         }
-        
+
+        private void EvaluateCharacterKeyPress(char key)
+        {
+            //Validate character...
+            if (!Calc.CharacterAllowed(key))
+            {
+                MessageBox.Show("Invalid Input. You entered: " + key.ToString());
+            }
+
+            //Remove leading zeros...
+            if (displayText.Length > 0 && displayText[0] == '0')
+            {
+                displayText.Remove(0, 1);
+            }
+
+            //Add character to stringbuilder...
+            displayText.Append(key);
+
+            //Update display with sb value.
+            WriteToDisplay(displayText.ToString());
+
+        }
+
         private void btnClear_Click(object sender, EventArgs e)
         {
             displayText.Clear();
@@ -95,7 +96,7 @@ namespace Calculator_1
         {
             Button btnWorking = (Button)sender;
             displayDirty = true;
-            UpdateDisplayString(Convert.ToChar(btnWorking.Text));
+            EvaluateCharacterKeyPress(Convert.ToChar(btnWorking.Text));
         }
 
         private void ButtonClickOperator(object sender, EventArgs e)
@@ -106,77 +107,12 @@ namespace Calculator_1
 
         private void WriteToDisplay(string textToDisplay)
         {
-            string result = textToDisplay;
-            bool decimalPresent = false;            
-            int decimalPosition = 0, leftOfDecimalLength = 0, rightOfDecimalLength = 0;
-
-            //Get Decimal Position, if any
-            if (textToDisplay.Contains('.')){
-                decimalPresent = true;
-                decimalPosition = textToDisplay.IndexOf('.');
-                leftOfDecimalLength = decimalPosition - 1;
-                rightOfDecimalLength = textToDisplay.Length - decimalPosition - 1;
-            }
-           
-            //limit to 20 places before or after decimal....
-            
-            //Develop display rules and check for violation....
-            if(leftOfDecimalLength > characterLimit || rightOfDecimalLength > characterLimit)
-            {
-                result = "num too big";
-            }
-            //evaluate string.  If length > 9 Use Exponential Notation 5.1234E4
-            else if (textToDisplay.Length > displayLimit && decimalPresent)
-            {
-                string e="error";
-
-                if (decimalPosition > 0 && decimalPosition < 7)
-                {
-                    e = (decimalPosition - 1).ToString();
-                }
-                else if(decimalPosition == 0)
-                {
-                    //where is the first non zero number?
-                    int c = 0;
-                    do
-                    {
-                        c += 1;
-                    } while (textToDisplay[c] !='0');
-                    e = c.ToString();                    
-                }
-
-                result = e;
-                
-            }
-            else if (textToDisplay.Length > displayLimit && !decimalPresent)
-            {
-                string Result;
-                const int eMaxDisplayLength = 5;
-                int displaySize = displayLimit > eMaxDisplayLength ? eMaxDisplayLength : displayLimit;
-
-                //Get the first 6 characters of string and convert to int
-                if (textToDisplay.Length > displaySize + 1)
-                {
-                    Result = textToDisplay.Remove(displaySize + 1);
-                }
-                else
-                {
-                    Result = textToDisplay;
-                }
-                //assume all positive...
-                double g = Convert.ToDouble(Result);
-                g = g/Math.Pow(10,displaySize); //(displayLimit > eDisplayLength ? 5 : displayLimit)
-                result = g.ToString() + "e" + (textToDisplay.Length-1).ToString();
-            }
-
-            
-
-            txtDisplay.Text = result;
+            txtDisplay.Text = textToDisplay;
         } 
 
         private void ClearDisplay()
         {
-            UpdateDisplayString('0');
+            EvaluateCharacterKeyPress('0');
         }
 
 
@@ -186,7 +122,7 @@ namespace Calculator_1
             if (Calc.CharacterAllowed(e.KeyChar))
             {
                 displayDirty = true;
-                UpdateDisplayString(e.KeyChar);
+                EvaluateCharacterKeyPress(e.KeyChar);
             }
             else if (Calc.OperatorAllowed(e.KeyChar))
             {
