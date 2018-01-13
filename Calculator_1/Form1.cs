ac831b6d90dd856022fd36dc8259ac99131bd00b
Jason Thatcher <contact@jasonthatcher.com>
2018-01-13 08:56:11 -0600

    Add display logic

--- Calculator_1/Form1.cs
+++ Calculator_1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace Calculator_1
 {
@@ -9,6 +10,8 @@ namespace Calculator_1
     {
         char ops;
         bool displayDirty;
+        const int characterLimit = 19; //limits the length of number left / right of decimal in calc...
+        const int displayLimit = 8; //Sets the display Limit will display exponent if over limit....
         CalculatorOps Calc;
         StringBuilder displayText;
 
@@ -33,7 +36,7 @@ namespace Calculator_1
             {
                 displayText.Remove(0, 1);
             }
-            
+
             //Add character to stringbuilder...
             displayText.Append(key);
 
@@ -103,8 +106,73 @@ namespace Calculator_1
 
         private void WriteToDisplay(string textToDisplay)
         {
-            txtDisplay.Text = textToDisplay;
-        }
+            string result = textToDisplay;
+            bool decimalPresent = false;            
+            int decimalPosition = 0, leftOfDecimalLength = 0, rightOfDecimalLength = 0;
+
+            //Get Decimal Position, if any
+            if (textToDisplay.Contains('.')){
+                decimalPresent = true;
+                decimalPosition = textToDisplay.IndexOf('.');
+                leftOfDecimalLength = decimalPosition - 1;
+                rightOfDecimalLength = textToDisplay.Length - decimalPosition - 1;
+            }
+           
+            //limit to 20 places before or after decimal....
+            
+            //Develop display rules and check for violation....
+            if(leftOfDecimalLength > characterLimit || rightOfDecimalLength > characterLimit)
+            {
+                result = "num too big";
+            }
+            //evaluate string.  If length > 9 Use Exponential Notation 5.1234E4
+            else if (textToDisplay.Length > displayLimit && decimalPresent)
+            {
+                string e="error";
+
+                if (decimalPosition > 0 && decimalPosition < 7)
+                {
+                    e = (decimalPosition - 1).ToString();
+                }
+                else if(decimalPosition == 0)
+                {
+                    //where is the first non zero number?
+                    int c = 0;
+                    do
+                    {
+                        c += 1;
+                    } while (textToDisplay[c] !='0');
+                    e = c.ToString();                    
+                }
+
+                result = e;
+                
+            }
+            else if (textToDisplay.Length > displayLimit && !decimalPresent)
+            {
+                string Result;
+                const int eMaxDisplayLength = 5;
+                int displaySize = displayLimit > eMaxDisplayLength ? eMaxDisplayLength : displayLimit;
+
+                //Get the first 6 characters of string and convert to int
+                if (textToDisplay.Length > displaySize + 1)
+                {
+                    Result = textToDisplay.Remove(displaySize + 1);
+                }
+                else
+                {
+                    Result = textToDisplay;
+                }
+                //assume all positive...
+                double g = Convert.ToDouble(Result);
+                g = g/Math.Pow(10,displaySize); //(displayLimit > eDisplayLength ? 5 : displayLimit)
+                result = g.ToString() + "e" + (textToDisplay.Length-1).ToString();
+            }
+
+            
+
+            txtDisplay.Text = result;
+        } 
 
         private void ClearDisplay()
         {
